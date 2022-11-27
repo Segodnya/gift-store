926f2ebc5ac8444af3416505801a92fb2d322b43
Kirill Kurentsov <immortalsteel@gmail.com>
2022-11-27 11:34:48 +0300

    feat: loading screen and gifts gallery

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
-const buttonGifts = document.querySelector(".button_place_gifts");
-const buttonBlogs = document.querySelector(".button_place_blogs");
-const giftsToHide = document.querySelectorAll(".gifts__card:not(:first-of-type)");
-const blogsToHide = document.querySelectorAll(".blogs__card");
-const hiddenGiftsLength = giftsToHide.length;
-const hiddenBlogsLength = blogsToHide.length;
+// Loader
 
-function toggleGifts() {
-  console.log(document.querySelectorAll(".gifts__card + .hidden").length);
-  giftsToHide.forEach((element) => {
-    element.classList.toggle("hidden");
-  });
-  if (document.querySelectorAll(".gifts__card + .hidden").length > 0) {
-    buttonGifts.textContent = `Show More (${hiddenGiftsLength})`;
-  } else {
-    buttonGifts.textContent = `Show Less`;
-  }
+let loader = document.querySelector(".loader");
+
+function sleep(ms = 2000) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+function hideLoader() {
+  loader.classList.add("hidden");
+}
+
+sleep(3000).then(() => {
+  hideLoader();
+});
+
+// Gifts
+
+let giftsCards = document.querySelectorAll(".gifts__card");
+let giftsPaginator = document.querySelector(".gifts__paginator");
+let nextButton = document.querySelector(".button_place_next");
+let prevButton = document.querySelector(".button_place_prev");
+let currentGift = 0;
+
+function showPaginator() {
+  giftsPaginator.textContent = `${currentGift + 1} / ${giftsCards.length}`;
+}
+
+function renderGiftsCard(currentGift) {
+  giftsCards.forEach((card) => {
+    card.classList.remove("gifts__card_active");
+  });
+  giftsCards[currentGift].classList.add("gifts__card_active");
+}
+
+function showNext() {
+  if (currentGift < giftsCards.length - 1) {
+    currentGift += 1;
+  } else {
+    currentGift = 0;
+  }
+  showPaginator();
+  renderGiftsCard(currentGift);
+}
+
+function showPrev() {
+  if (currentGift === 0) {
+    currentGift = giftsCards.length - 1;
+  } else {
+    currentGift -= 1;
+  }
+  showPaginator();
+  renderGiftsCard(currentGift);
+}
+
+showPaginator();
+nextButton.addEventListener("click", showNext);
+prevButton.addEventListener("click", showPrev);
+
+// Blogs
+
+const buttonBlogs = document.querySelector(".button_place_blogs");
+const blogsToHide = document.querySelectorAll(".blogs__card");
+const hiddenBlogsLength = blogsToHide.length;
 
 function toggleBlogs() {
   blogsToHide.forEach((element) => {
@@ -29,10 +75,8 @@ function toggleBlogs() {
 }
 
 function showHowManyHiddenElements() {
-  buttonGifts.textContent = `Show More (${hiddenGiftsLength})`;
   buttonBlogs.textContent = `Show More (${hiddenBlogsLength})`;
 }
 
 showHowManyHiddenElements();
-buttonGifts.addEventListener("click", toggleGifts);
 buttonBlogs.addEventListener("click", toggleBlogs);
